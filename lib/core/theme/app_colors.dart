2d6fff8720ef85ca1e1b4084acbf4a4a36a6488f
Zahra Sedighi <zahrasedighi2004@gmail.com>
2026-06-07 10:41:39 +0330

    Initial commit: Setup project structure

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF4C4DDC);   // 01
+  static const Color surface = Color(0xFFEDEDFC);   // 02
+  static const Color dark = Color(0xFF101010);      // 03
+  static const Color secondary = Color(0xFFC8C8F4); // 04
+  static const Color neutral = Color(0xFFD4D4D8);   // 05
+
+}
