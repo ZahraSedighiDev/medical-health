f263ba818267dbd9bfcddaf5d0f0b6120dcbe2d0
Zahra Sedighi <zahrasedighi2004@gmail.com>
2026-06-07 10:48:51 +0330

    app icon size  was created

---
+++ lib/core/theme/app_icon_size.dart
@@ -0,0 +1,25 @@
+
+class AppIconSize {
+  const AppIconSize._();
+
+// total size for svg and icons
+  static const double t1 = 10 ;
+  static const double t2 = 19 ;
+  static const double t3 = 25 ;
+  static const double t4 =40 ;
+
+//height size for svg and icons
+  static const double  h1 = 40 ;
+  static const double h2 = 100;
+  static const double h3 = 200;
+  static const double h6 = 500;
+
+// width size for svg and icon
+  static const double w1 =20 ;
+  static const double w2 = 40;
+  static const double w3 = 80 ;
+  static const double w4 = 100;
+  static const double w5 = 160 ;
+  static const double w6 = 300;
+  static const double w10 = 600;
+}
