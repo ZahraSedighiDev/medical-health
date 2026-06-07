9116c51cd928c8efee54e3416d33d819f0fa0eea
Zahra Sedighi <zahrasedighi2004@gmail.com>
2026-06-07 10:50:21 +0330

    app icon  was created

---
+++ lib/core/theme/app_icons.dart
@@ -0,0 +1,19 @@
+
+class AppIcons {
+  const AppIcons._();
+//pics
+  static const String doctor1 = 'assets/pics/Doctor1.png' ;
+  static const String doctor2 = 'assets/pics/Doctor2.png' ;
+  static const String doctor3 = 'assets/pics/Doctor3.png' ;
+  static const String doctor4 = 'assets/pics/Doctor4.png' ;
+  static const String doctor5 = 'assets/pics/Doctor5.png' ;
+//svg
+  static const String doctor11 = 'assets/pics/Doctor1.svg' ;
+  static const String doctor12 = 'assets/pics/Doctor2.svg' ;
+  static const String doctor13 = 'assets/pics/Doctor3.svg' ;
+  static const String doctor14 = 'assets/pics/Doctor4.svg' ;
+  static const String doctor15 = 'assets/pics/Doctor5.svg' ;
+
+
+
+}
